d9ae4a28da1cdef9d9b46f9fda45b62a7c291100
Cyril <cyril.code@hotmail.com>
2023-07-20 11:30:50 +0200

    Update PoliceAPI_Client.sln, App.xaml, and 47 more files...

---
+++ TestMap/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestMap
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TestMap/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using PoliceAPI_Client.Services.Implementations;
+using PoliceAPI_Client.Services.Interfaces;
+
+namespace TestMap
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+            builder.Services.AddSingleton<ICrimeService, CrimeService>();
+
+            return builder.Build();
+        }
+    }
+}
